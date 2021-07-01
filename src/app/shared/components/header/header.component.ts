fbf649e5c17d49c7695529ccce5956ec214cfb17
Daniel Córdova <jdany0809@hotmail.com>
2021-07-01 14:06:09 -0500

    implementation of animations, logo and responsive design

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,6 +10,25 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready(function(){
+      const burger = document.querySelector('.three_lines');
+      const nav = document.querySelector('.nav_menu');
+      const navOptions = document.querySelectorAll('.nav_menu li')
+      console.log(navOptions)
+
+      burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+        navOptions.forEach((link,index) => {
+          console.log(nav.classList.contains('nav-active'))
+          if(nav.classList.contains('nav-active')){
+            let style = `animation:navOptFadeAnimation 0.5s ease forwards ${index / 7 + 0.5}s;`
+            link.setAttribute('style', style)
+          }else{
+            link.setAttribute('style', '')
+          }
+        })
+      })
+    })
   }
 
 }
